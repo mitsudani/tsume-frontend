4c59dc7d6b1e7b287e088258052d4c2aa9555465
Mitsudani <mdsachetti@gmail.com>
2022-07-25 02:45:27 -0300

    Finish Button story. SASS variables and mixins names refactor

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,59 @@
+import { ReactElement, ReactNode } from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Link.module.scss";
+
+export enum LinkVariant {
+  Primary,
+  Secondary,
+  ButtonPrimary,
+  ButtonSecondary,
+}
+
+const stylesMap = {
+  [LinkVariant.Primary]: styles.primary,
+  [LinkVariant.Secondary]: styles.secondary,
+  [LinkVariant.ButtonPrimary]: styles.button,
+  [LinkVariant.ButtonSecondary]: styles.buttonSecondary,
+};
+
+interface LinkProps {
+  children?: ReactNode;
+  to: string;
+  className?: string;
+  variant?: LinkVariant;
+  [x: string]: unknown;
+}
+
+const Link = ({
+  children,
+  to,
+  className,
+  variant = LinkVariant.Primary,
+  ...attributes
+}: LinkProps): ReactElement => {
+  if (!to.startsWith("http")) {
+    return (
+      <NavLink
+        to={to}
+        {...attributes}
+        className={`${stylesMap[variant]} ${className}`}
+      >
+        {children}
+      </NavLink>
+    );
+  }
+
+  return (
+    <a
+      href={to}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`${stylesMap[variant]} ${className}`}
+      {...attributes}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default Link;
---
+++ src/components/Link/index.tsx
@@ -0,0 +1,1 @@
+export { default, LinkVariant } from "./Link";
