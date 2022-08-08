5bcb54ee0ee87409d561d1b9c9a79995b7ab1c9a
Mitsudani <mdsachetti@gmail.com>
2022-08-07 23:41:04 -0300

    HeaderNavbar component created

---
+++ src/components/HeaderNavbar/HeaderNavbar.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+import { HOME, HOW_IT_WORKS } from "../../constants/routes";
+import styles from "./HeaderNavbar.module.scss";
+
+const HeaderNavbar = (): JSX.Element => {
+  return (
+    <nav className={styles.container} data-testid="navbar">
+      <ul>
+        <li>
+          <NavLink
+            to={HOME}
+            className={({ isActive }) => (isActive ? "activeLink" : "")}
+            data-testid="header-navbar-home-link"
+          >
+            HOME
+          </NavLink>
+        </li>
+        <li className={styles.separator}>|</li>
+        <li>
+          <NavLink
+            to={HOW_IT_WORKS}
+            className={({ isActive }) => (isActive ? "activeLink" : "")}
+            data-testid="header-navbar-howItWorks-link"
+          >
+            HOW IT WORKS
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default HeaderNavbar;
