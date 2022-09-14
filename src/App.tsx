a21ba992c265c501f32f88494b116379e1b04615
Mitsudani <mdsachetti@gmail.com>
2022-09-13 23:27:58 -0300

    Container mixin created. Some changes to App and Navbar css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import "./index.scss";
+import Layout from "./components/Layout";
 
-function App() {
-  return <div>TSUME</div>;
+function App(): JSX.Element {
+  const [isReady, setIsReady] = useState(false);
+  (document as any).fonts.load("16px Mukta");
+  (document as any).fonts.load("16px Ms Madi");
+  (document as any).fonts.ready.then(() => setIsReady(true));
+
+  return isReady ? (
+    <div className="App">
+      <Layout />
+    </div>
+  ) : (
+    <div>TODO LOADER COMPONENT</div>
+  );
 }
 
 export default App;
