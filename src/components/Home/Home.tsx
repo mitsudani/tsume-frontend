b4e15fdfabccaab04e5372271c3b1edd66ae58d6
Mitsudani <mdsachetti@gmail.com>
2022-09-25 23:27:14 -0300

    Home base component created. Changes to Layout and Footer

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import styles from "./Home.module.scss";
+
+const Home = (): JSX.Element => {
+  return <main className={styles.container}>HOME</main>;
+};
+
+export default Home;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "../Header";
 import ScrollToTop from "../ScrollToTop";
 import Footer from "../Footer";
+import Home from "../Home";
 import {
   HOME,
   LOGIN,
@@ -16,7 +17,9 @@ const Layout = (): JSX.Element => (
   <BrowserRouter>
     <Header />
     <ScrollToTop>
-      <Routes>{/* <Route path={HOME} element={<Home/>} /> */}</Routes>
+      <Routes>
+        <Route path={HOME} element={<Home />} />
+      </Routes>
     </ScrollToTop>
     <Footer />
   </BrowserRouter>
