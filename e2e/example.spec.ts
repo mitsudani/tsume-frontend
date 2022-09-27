18930b74a173f61420dd15da4bfadfba5c4c4392
Mitsudani <mdsachetti@gmail.com>
2022-09-27 02:08:13 -0300

    Page title updated. Example test modified.

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,20 +1,22 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("homepage has Tsume in title and How It Works link linking to the that page", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:3000/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Tsume/);
 
   // create a locator
-  const getStarted = page.locator('text=Get Started');
+  const getStarted = page.locator("text=How It Works");
 
   // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+  await expect(getStarted).toHaveAttribute("href", "/how-it-works");
 
   // Click the get started link.
-  await getStarted.click();
+  // await getStarted.click();
 
   // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  // await expect(page).toHaveURL(/.*intro/);
 });
