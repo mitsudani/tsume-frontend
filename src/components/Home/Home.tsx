025722f29596f9a4a6f1c58c4a92fbabc4a64d15
Mitsudani <mdsachetti@gmail.com>
2022-10-14 13:11:59 -0300

    Home component css and other changes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,25 @@
-import { Link } from "react-router-dom";
 import styles from "./Home.module.scss";
+import LinkComponent, { LinkVariant } from "../Link";
+import { LOGIN } from "../../constants/routes";
 
 const Home = (): JSX.Element => {
     return (
         <main className={styles.container}>
             <div className={styles.titleContainer}>
                 <h1>Tsume</h1>
-                <div>
-                    <p>The complete tracker</p>
-                    <p>for nail artists</p>
-                </div>
+                <h2>
+                    The complete <span>tracker</span> <br />
+                    for <span>nail artists</span>
+                </h2>
             </div>
+            <div className={styles.line}></div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.
+            </p>
+            <LinkComponent to={LOGIN} variant={LinkVariant.ButtonPrimary} data-testid="get-started-link">
+                Get Started
+            </LinkComponent>
         </main>
     );
 };
