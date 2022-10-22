01d5933b55dd7ff7f25aea7a790d02d49ac91601
Mitsudani <mdsachetti@gmail.com>
2022-10-21 22:55:42 -0300

    Css names refactor

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -21,18 +21,28 @@ const Home = (): JSX.Element => {
                 Get Started
             </LinkComponent>
             <div className={styles.backgroundLeft}>
-                <span className={styles.circleLilac1}></span>
+                <span className={styles.circleLilacLeft1}></span>
+                <span className={styles.circleLilacLeft2}></span>
+                <span className={styles.circleLilacLeft3}></span>
+                <span className={styles.circleLilacLeft4}></span>
+                <span className={styles.circleLilacLeft5}></span>
+                <span className={styles.circleWhiteLeft1}></span>
+                <span className={styles.circleWhiteLeft2}></span>
+                <span className={styles.circleWhiteLeft3}></span>
+                <span className={styles.circleWhiteLeft4}></span>
+                <span className={styles.circleWhiteLeft5}></span>
+                <span className={styles.circleWhiteLeft6}></span>
+                <span className={styles.circleWhiteLeft7}></span>
+            </div>
+            <div className={styles.backgroundRight}>
+                <span className={styles.circleLilacRight1}></span>
                 <span className={styles.circleLilac2}></span>
                 <span className={styles.circleLilac3}></span>
-                <span className={styles.circleLilac4}></span>
-                <span className={styles.circleLilac5}></span>
                 <span className={styles.circleWhite1}></span>
                 <span className={styles.circleWhite2}></span>
                 <span className={styles.circleWhite3}></span>
                 <span className={styles.circleWhite4}></span>
                 <span className={styles.circleWhite5}></span>
-                <span className={styles.circleWhite6}></span>
-                <span className={styles.circleWhite7}></span>
             </div>
         </main>
     );
